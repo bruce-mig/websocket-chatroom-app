ec274bb9164e29761ef887bd33f2c518e74b0642
bruce-mig <brucetmig@icloud.com>
2024-11-04 21:10:05 +0200

    first commit

---
+++ src/test/java/com/github/bruce_mig/websockets/WebsocketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bruce_mig.websockets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
